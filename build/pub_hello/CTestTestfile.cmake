c7626cb8c81eb33111b3dc64fdb3fd945141b1b9
Syst3m-R0ot <128175204+Syst3mR0ot@users.noreply.github.com>
2023-05-20 12:52:31 +0300

    Delete build directory

--- build/pub_hello/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/system-r0ot/robot1_ws/src/pub_hello
-# Build directory: /home/system-r0ot/robot1_ws/build/pub_hello
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
--- build/robot1_pkg/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/system-r0ot/robot1_ws/src/robot1_pkg
-# Build directory: /home/system-r0ot/robot1_ws/build/robot1_pkg
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
